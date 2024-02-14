8868f6c2e975dea6855c6385355e9737fa5cf978
UlanTursunbek <ulantursunbekdev@gmail.com>
2024-02-14 15:15:17 +0600

    Configure development enviroments

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 7000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(PORT, () =>
+    console.log(`Server is running on port ${PORT}`),
+  );
 }
+
 bootstrap();
